cab79e703f535f6b59a24df51ad513e78e538c4d
Selman Yıldız <selmanyldz62@gmail.com>
2021-01-30 20:56:00 +0300

    My third commid

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello World Test");
-        
+        System.out.println("EU4 is going great!");
     }
 }
